f73129f7845bedd730c4e7a10e58af48a27fd147
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-12-26 10:30:21 -0500

    Show world state in browser

--- src/elevator/views.cljs
+++ src/elevator/views.cljs
@@ -1,5 +1,73 @@
-(ns elevator.views)
+(ns elevator.views
+  (:require
+    [elevator.state :as state]))
+
+(defn game-view [world-state]
+  (let [floor-height 50
+        waiting-area-width 100
+        elevator-width 35
+        elevator-gap 5
+        total-height (* floor-height (- (world-state :floor-count) 1))] 
+    [:svg
+     (for [floor-index (range (world-state :floor-count))]
+       ^{:key floor-index}
+       [:g {:transform (str "translate(" 0 "," (- total-height (* floor-height floor-index)) ")")}
+        [:rect.floor 
+         {:width (+ waiting-area-width (* (count (world-state :elevators)) (+ elevator-width elevator-gap))) 
+          :height floor-height 
+          :fill "#999" 
+          :stroke "black" 
+          :stroke-width "2px"}]
+        [:g
+         [:rect.waiting-area
+          {:width waiting-area-width
+           :height floor-height
+           :fill "#666"}]
+         (map-indexed 
+           (fn [index person]
+             ^{:key index}
+             [:circle
+              {:cx (- waiting-area-width 20 (* index 25))
+               :cy 20
+               :r 10
+               :fill "yellow"
+               :stroke "black"
+               :stroke-width "1px"}]) 
+           (->> (world-state :people)
+                (filter (fn [person]
+                          (= {:floor floor-index} (person :location))))))]])
+     (for [elevator (world-state :elevators)]
+       ^{:key (elevator :index)}
+       [:g {:style {:transition "transform 1s ease-in-out"
+                    :transform (str "translate(" (+ waiting-area-width (* (elevator :index) (+ elevator-width elevator-gap))) "px ,"
+                                    (- total-height (* floor-height (elevator :floor))) "px)")}}
+        [:rect.elevator 
+         {:width elevator-width 
+          :height floor-height 
+          :fill "black" 
+          :stroke "black" 
+          :stroke-width "2px"}]
+        (map-indexed 
+          (fn [index person]
+            ^{:key index}
+            [:circle
+             {:cx (+ 20 (* index 5))
+              :cy 20
+              :r 10
+              :fill "yellow"
+              :stroke "black"
+              :stroke-width "1px"}]) 
+          (->> (world-state :people)
+               (filter (fn [person]
+                         (= {:elevator (elevator :index)} (person :location))))))])]))
 
 (defn app-view []
-  [:div
-   "hello"])
+  [game-view
+   (state/run {:floor-count 2
+               :elevator-count 3
+               :ticks 4
+               :people-generator (fn [_] 
+                                   [{:floor 1
+                                     :target-floor 0}])
+               :elevator-logic (fn [_] {0 :up})})])
+
