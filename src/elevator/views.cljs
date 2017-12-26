1f1392c34b8efd455de453d1aa1b2210ddaf0eb4
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-12-26 13:05:11 -0500

    Add buttons for step-by-step history traversal

--- src/elevator/views.cljs
+++ src/elevator/views.cljs
@@ -26,5 +26,16 @@
              :value (@state :tick)
              :on-change (fn [e]
                           (swap! state assoc :tick (js/parseInt (.. e -target -value) 10)))}]
+
+    [:button {:on-click (fn [_]
+                          (when (> (@state :tick) 0)
+                            (swap! state update :tick dec)))} 
+     "<"]
+
+    [:button {:on-click (fn [_]
+                          (when (< (@state :tick) (dec (count (@state :world-states))))
+                            (swap! state update :tick inc)))} 
+     ">"]
+
     (@state :tick)]])
 
